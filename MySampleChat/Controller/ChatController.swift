fe83c997f1e23b25e860dc370191f34a35cf5d55
Unnikrishnan <unni@iris-llc.com>
2019-02-02 15:09:36 +0530

    Updated

--- MySampleChat/Controller/ChatController.swift
+++ MySampleChat/Controller/ChatController.swift
@@ -13,38 +13,85 @@ import FirebaseDatabase
 import SDWebImage
 
 class ChatController: UICollectionViewController ,UITextFieldDelegate,UICollectionViewDelegateFlowLayout{
-   
+    
     var txtField  =  UITextField()
     var user = [User]()
     var messages = [Message]()
     var messageDict = [String: Message]()
     var users : User?
+    var containerViewBottom : NSLayoutConstraint?
+    let sampleViewc = UIView()
     
- 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = users?.name
-  navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Log out", style: .plain, target: self, action: #selector(handleLogout))
-        setupInputComponent()
-     observeMessages()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Log out", style: .plain, target: self, action: #selector(handleLogout))
+        
+        observeMessages()
         collectionView.register(FreelancerCell.self, forCellWithReuseIdentifier: "Cell")
         collectionView.showsVerticalScrollIndicator = false
-        collectionView.contentInset = UIEdgeInsets(top: 8, left: 2, bottom: 58, right: 2)
+        collectionView.contentInset = UIEdgeInsets(top: 8, left: 0, bottom: 58, right: 0)
         collectionView.scrollIndicatorInsets = UIEdgeInsets(top: 00, left: 0, bottom: 58, right: 0)
         collectionView.backgroundColor = .white
+        collectionView.keyboardDismissMode = .interactive
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        setupInputComponent()
+        sampleViewc.frame = self.view.frame
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self)
+    }
+    @objc func keyboardWillShow(notification: Notification) {
+        if   let duration = notification.userInfo![UIResponder.keyboardAnimationDurationUserInfoKey] as? Double{
+            if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+                print("notification: Keyboard will show")
+              
+                    self.view.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - keyboardSize.height)
+//                    self.view.frame.origin.y -= keyboardSize.height
+                    UIView.animate(withDuration: duration) {
+                        self.view.layoutIfNeeded()
+                    }
+                
+                if self.collectionView.numberOfItems(inSection: 0) > 0{
+                            let lastItemIndex = self.collectionView.numberOfItems(inSection: 0) - 1
+                            let indexPath:IndexPath = IndexPath(item: lastItemIndex, section: 0)
+                            self.collectionView.scrollToItem(at: indexPath, at: .bottom, animated: false)
+                            }
+            }
+        }
+        
+    }
+    
+    @objc func keyboardWillHide(notification: Notification) {
+        if   let duration = notification.userInfo![UIResponder.keyboardAnimationDurationUserInfoKey] as? Double{
+          
+                self.view.frame = sampleViewc.frame
+                UIView.animate(withDuration: duration) {
+                    self.view.layoutIfNeeded()
+                }
+                if self.collectionView.numberOfItems(inSection: 0) > 0{
+                    let lastItemIndex = self.collectionView.numberOfItems(inSection: 0) - 1
+                    let indexPath:IndexPath = IndexPath(item: lastItemIndex, section: 0)
+                    self.collectionView.scrollToItem(at: indexPath, at: .bottom, animated: false)
+            
+            }
+        }
     }
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         collectionView.collectionViewLayout.invalidateLayout()
     }
     func setupInputComponent(){
         let containerView = UIView()
-        containerView.backgroundColor = .clear
+        containerView.backgroundColor = .white
         containerView.translatesAutoresizingMaskIntoConstraints = false
-         view.addSubview(containerView)
+        view.addSubview(containerView)
         collectionView?.backgroundColor = .white
         
         containerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        containerViewBottom =   containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        containerViewBottom!.isActive = true
         containerView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         containerView.heightAnchor.constraint(equalToConstant: 50).isActive = true
         let sendBtn = UIButton()
@@ -58,6 +105,7 @@ class ChatController: UICollectionViewController ,UITextFieldDelegate,UICollecti
         sendBtn.heightAnchor.constraint(equalTo: containerView.heightAnchor).isActive = true
         sendBtn.widthAnchor.constraint(equalToConstant: 80).isActive = true
         txtField.placeholder = "Enter text.."
+        txtField.delegate = self
         txtField.translatesAutoresizingMaskIntoConstraints = false
         containerView.addSubview(txtField)
         txtField.leftAnchor.constraint(equalTo: containerView.leftAnchor).isActive = true
@@ -77,6 +125,7 @@ class ChatController: UICollectionViewController ,UITextFieldDelegate,UICollecti
     @objc func handleLogout(){
         navigationController?.popViewController(animated: true)
     }
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         BtnSend()
         return true
@@ -84,44 +133,45 @@ class ChatController: UICollectionViewController ,UITextFieldDelegate,UICollecti
     func observeMessages(){
         messages.removeAll()
         messageDict.removeAll()
-               if ((Auth.auth().currentUser?.uid) != nil){
-                let msgRef = Database.database().reference().child("user-messages").child((Auth.auth().currentUser?.uid)!).child((users?.id)!)
-        msgRef.observe(.childAdded, with: { (snapshot) in
-            let key = snapshot.key
-            let  ref =  Database.database().reference().child("messages").child(key)
-            ref.observeSingleEvent(of: .value, with: { (snapshot) in
-                print(snapshot)
-                if let dict = snapshot.value as? [String:AnyObject]{
-                    print(dict)
-                    let fromId = dict["fromId"] as? String
-                    let toId = dict["toId"] as? String
-                    let text = dict["text"] as? String
-                    let timestamp = dict["timestamp"] as? NSNumber
-                    let dataMessge =   Message(fromId: fromId!, toId: toId!, text: text!, timestamp: timestamp!)
-                   
-                    if dataMessge.chatPartnerID()  == self.users?.id{
-                         self.messages.append(dataMessge)
-                        DispatchQueue.main.async {
-                            self.collectionView.reloadData()
+        if ((Auth.auth().currentUser?.uid) != nil){
+            let msgRef = Database.database().reference().child("user-messages").child((Auth.auth().currentUser?.uid)!).child((users?.id)!)
+            msgRef.observe(.childAdded, with: { (snapshot) in
+                let key = snapshot.key
+                let  ref =  Database.database().reference().child("messages").child(key)
+                ref.observeSingleEvent(of: .value, with: { (snapshot) in
+                    print(snapshot)
+                    if let dict = snapshot.value as? [String:AnyObject]{
+                        print(dict)
+                        let fromId = dict["fromId"] as? String
+                        let toId = dict["toId"] as? String
+                        let text = dict["text"] as? String
+                        let timestamp = dict["timestamp"] as? NSNumber
+                        let dataMessge =   Message(fromId: fromId!, toId: toId!, text: text!, timestamp: timestamp!)
+                        
+                        if dataMessge.chatPartnerID()  == self.users?.id{
+                            self.messages.append(dataMessge)
+                            DispatchQueue.main.async {
+                                self.collectionView.reloadData()
+                            }
                         }
                     }
-                    
-                   
-                }
+                }, withCancel: nil)
+                
             }, withCancel: nil)
-            
-        }, withCancel: nil)
         }
     }
     @objc func BtnSend() {
+        guard  txtField.text  != "" else {
+            return
+        }
         let ref = Database.database().reference().child("messages")
         let childRef = ref.childByAutoId()
         let toID = users?.id
         let fromID =  Auth.auth().currentUser?.uid
-         let timestamp: NSNumber = (Date().timeIntervalSince1970 as AnyObject as! NSNumber)
+        let timestamp: NSNumber = (Date().timeIntervalSince1970 as AnyObject as! NSNumber)
         let values = ["text": txtField.text!,"toId":toID! as Any, "fromId": fromID!,"timestamp": timestamp] as [String : Any]
         childRef.updateChildValues(values)
-      self.txtField.text = nil
+        self.txtField.text = nil
         guard let messageId = childRef.key else { return }
         
         let userMessagesRef = Database.database().reference().child("user-messages").child(fromID!).child(toID!).child(messageId)
@@ -130,6 +180,14 @@ class ChatController: UICollectionViewController ,UITextFieldDelegate,UICollecti
         let recipientUserMessagesRef = Database.database().reference().child("user-messages").child(toID!).child(fromID!).child(messageId)
         recipientUserMessagesRef.setValue(1)
         
+       
+       
+        if self.collectionView.numberOfItems(inSection: 0) > 0{
+        let lastItemIndex = self.collectionView.numberOfItems(inSection: 0) - 1
+        let indexPath:IndexPath = IndexPath(item: lastItemIndex, section: 0)
+        self.collectionView.scrollToItem(at: indexPath, at: .bottom, animated: false)
+        }
+       
         
     }
     
@@ -141,28 +199,28 @@ class ChatController: UICollectionViewController ,UITextFieldDelegate,UICollecti
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! FreelancerCell
         let data = messages[indexPath.row]
         let urlImage  = URL(string: (users?.profileURL)!)
-         cell.profileImageview.sd_setShowActivityIndicatorView(true)
-         cell.profileImageview.sd_setIndicatorStyle(.whiteLarge)
+        cell.profileImageview.sd_setShowActivityIndicatorView(true)
+        cell.profileImageview.sd_setIndicatorStyle(.whiteLarge)
         
         cell.profileImageview.sd_setImage(with: urlImage , placeholderImage: DEFAULT_USER_IMAGE)
         if data.fromId ==  Auth.auth().currentUser?.uid{
-              cell.textView.backgroundColor = .blue
+            cell.textView.backgroundColor = .blue
             cell.profileImageview.isHidden = true
         }else{
-             cell.textView.backgroundColor = .gray
+            cell.textView.backgroundColor = .gray
             cell.rightAnchors?.isActive = false
             cell.leftAnchors?.isActive = true
         }
-      
         
-      cell.textView.text = data.text
+        
+        cell.textView.text = data.text
         cell.widthConstrain?.constant = EstimatedHeight(text: data.text!).width + 30
-    
+        
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         var height:CGFloat = 80
-          let data = messages[indexPath.row]
+        let data = messages[indexPath.row]
         if let text = data.text{
             height = EstimatedHeight(text: text).height + 20
         }
@@ -174,7 +232,7 @@ class ChatController: UICollectionViewController ,UITextFieldDelegate,UICollecti
         let options = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
         return NSString(string: text).boundingRect(with: size, options: options, attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18)], context: nil)
     }
-
+    
 }
 class FreelancerCell: UICollectionViewCell {
     let textView: UITextView = {
@@ -184,7 +242,7 @@ class FreelancerCell: UICollectionViewCell {
         textView.font = UIFont.boldSystemFont(ofSize: 18)
         textView.textAlignment = .center
         textView.clipsToBounds = true
-         textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.translatesAutoresizingMaskIntoConstraints = false
         return textView
     }()
     let profileImageview: UIImageView = {
@@ -195,12 +253,12 @@ class FreelancerCell: UICollectionViewCell {
         return profileImageview
     }()
     var widthConstrain : NSLayoutConstraint?
-       var rightAnchors : NSLayoutConstraint?
+    var rightAnchors : NSLayoutConstraint?
     var leftAnchors : NSLayoutConstraint?
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(textView)
-         addSubview(profileImageview)
+        addSubview(profileImageview)
         profileImageview.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
         profileImageview.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
         profileImageview.heightAnchor.constraint(equalToConstant: 32).isActive = true
@@ -209,7 +267,7 @@ class FreelancerCell: UICollectionViewCell {
         rightAnchors =  textView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -20)
         rightAnchors!.isActive = true
         leftAnchors =  textView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 50)
-//        leftAnchors!.isActive = true
+        //        leftAnchors!.isActive = true
         textView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
         textView.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
         widthConstrain =  textView.widthAnchor.constraint(equalToConstant: 200)
